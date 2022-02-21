bba78c4d9ffc08f9b0865e312c1a3c409c8b4b1f
Mohammad Dadkhah <mohipier@gmail.com>
2022-02-21 18:56:42 +0330

    edit NFT.sol file

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -15,5 +15,11 @@ contract NFT is ERC721URIStorage {
 
 	function createToken(string memory tokenURI) public returns (uint) {
 		_tokenIds.increment();
+		uint256 newItemId = _tokenIds.current();
+
+		_mint(msg.sender, newItemId);
+		_setTokenURI(newItemId , tokenURI);
+		setApprovalForAll(contractAddress, true);
+		return newItemId;
 	}
 }
