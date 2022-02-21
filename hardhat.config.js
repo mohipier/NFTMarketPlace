f887c5bad74b367b15a7d6d3e7529bd9ca86ea38
Mohammad Dadkhah <mohipier@gmail.com>
2022-02-21 18:45:02 +0330

    create structure and add first NFT smartcontract code

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();;
+const projectId = "4f6778dce2a344559f91f99adc0d6a6a";
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat:{
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
