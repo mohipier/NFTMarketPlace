5659742591e09e7035e49f2ecbf1953e7dcc6fd6
Mohammad Dadkhah <mohipier@gmail.com>
2022-02-25 16:52:43 +0330

    add create item cretor dashboard and my assets and change some code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers"
 import { useEffect, useState } from "react"
 import axios from "axios"
-import web3Modal from 'web3modal'
+import Web3Modal from 'web3modal'
 
 import { nftaddress, nftmarketaddress } from "../config"
 
@@ -17,15 +17,15 @@ export default function Home() {
   }, []);
 
   async function loadNFTs() {
-    const provider = new ethers.providers.JsonRpcBatchProvider();
+    const provider = new ethers.providers.JsonRpcBatchProvider("https://rpc-mumbai.matic.today");
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems();
 
     const items = await Promise.all(data.map(async i => {
-      const tokenUri = await tokentContract.tokenURI(i.tokenId);
+      const tokenUri = await tokenContract.tokenURI(i.tokenId);
       const meta = await axios.get(tokenUri);
-      let price = ethers.utils.formatUrils(i.price.toString(), 'ether');
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
       let item = {
         price,
         tokenId: i.tokenId.toNumber(),
@@ -44,7 +44,7 @@ export default function Home() {
   }
 
   async function buyNft(nft){
-    const web3Modal = new web3Modal();
+    const web3Modal = new Web3Modal();
     const connection = await web3Modal.connect();
     const provider = new ethers.providers.Web3Provider(connection);
 
@@ -69,7 +69,7 @@ export default function Home() {
       <div className="px-4" style={{ maxWidth: '1600px' }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {
-            nfts.map((nft, i) => {
+            nfts.map((nft, i) => (
                 <div key={i} className="border shadow rounded-xl overflow-hidden">
                   <img src={nft.image} />
                   <div className="p-4">
@@ -85,7 +85,7 @@ export default function Home() {
                     >Buy</button>
                   </div>
               </div>
-            })
+            ))
           }
         </div>
       </div>
